17ded665a07cf20207e264c9b56884f9fa4731d0
adiaz <agustin.gallici@gmail.com>
2024-07-13 20:30:32 -0300

    Predicate function

--- modules/src/main/java/com/platzi/functional/_04_functional/Mathfunctions.java
+++ modules/src/main/java/com/platzi/functional/_04_functional/Mathfunctions.java
@@ -1,19 +1,44 @@
 package com.platzi.functional._04_functional;
 
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class Mathfunctions {
     public static void main(String[] args) {
-        Function<Integer,Integer> squareFunction = new Function<Integer, Integer>() {
+        Function<Integer,Integer> squareFunction =
+                new Function<Integer, Integer>() {
             @Override
             public Integer apply(Integer x) {
                 return x*x;
             }
         };
 
-        System.out.println(squareFunction.apply(5));
-        System.out.println(squareFunction.apply(25));
-        System.out.println(square(5));
+//        System.out.println(squareFunction.apply(5));
+//        System.out.println(squareFunction.apply(25));
+//        System.out.println(square(5));
+
+        Function<Integer,Boolean> isOdd = x -> x % 2 == 1;
+
+        Predicate<Integer> isEven = x -> x % 2 == 0; //una function que testea si algo es valido
+
+        isEven.test(4); //true
+
+        Predicate<Student> isApproved = student -> student.getCalificacion() >= 6.0;
+
+        Student agus = new Student(5.9);
+        System.out.println("El estudiante is approved: " + isApproved.test(agus));
+
+    }
+
+    static class Student{
+        private double calificacion;
+
+        public Student(double calificacion){
+            this.calificacion = calificacion;
+        }
+        public double getCalificacion() {
+            return calificacion;
+        }
     }
 
     static int square(int x) {
